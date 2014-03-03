28cd8673cf66406c9ecea4168b39dc3a44c61af1
dandrory <Dan.drory@Hotmail.nl>
2014-03-03 15:00:53 +0100

    Initial Unity Commit
    Working from this base file/folder is required

---
+++ UnityProject/Assets/Scripts/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Test Comment in start
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Test Comment in Update
+	}
+}
