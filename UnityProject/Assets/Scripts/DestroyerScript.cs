3f203ea3ccbdb691b9c5070ce7b11a505875295a
DavidWillemstein <davidwillemstein@hotmail.com>
2014-03-12 11:58:07 +0100

    Spawner, sweeper, deel score

---
+++ UnityProject/Assets/Scripts/DestroyerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyerScript : MonoBehaviour {
+
+	private void OnTriggerStay(Collider col)
+	{
+		Debug.Log ("Hit");
+
+		if (col.gameObject.tag == "obstacle")
+		{
+			Debug.Log("Hit");
+			Destroy(col.gameObject);
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/HUDScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+	
+	float playerScore = 0;
+	
+	void Update (){
+		playerScore += Time.deltaTime;
+	}
+	
+	public void IncreaseScore(int amount)
+	{ 
+		playerScore += amount;
+	}
+	
+	void OnGUI()
+	{
+		GUI.Label (new Rect(10, 10, 100, 30), "Score:" +(int) (playerScore * 100));
+	}
+}
