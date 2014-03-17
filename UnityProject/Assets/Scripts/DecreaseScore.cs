ebc1bd7121513eb7493b917d29635cdc00d0f416
wolverr001 <ruud.wolvers@hva.nl>
2014-03-17 13:21:05 +0100

    + - score
    jep

---
+++ UnityProject/Assets/Scripts/DecreaseScore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecreaseScore : MonoBehaviour {
+	HUDScript hud;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			hud = GameObject.Find("_ScoreHandler").GetComponent<HUDScript> ();
+			hud.IncreaseScore(-1000);
+			Destroy (this.gameObject);
+			
+			
+			
+		}
+	}
+}
