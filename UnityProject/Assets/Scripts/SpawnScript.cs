4f9df652e9d8f8a57fe805ef8a61f3dfc310716c
DavidWillemstein <davidwillemstein@hotmail.com>
2014-03-17 12:51:21 +0100

    omdat het moet

--- UnityProject/Assets/Scripts/SpawnScript.cs
+++ UnityProject/Assets/Scripts/SpawnScript.cs
@@ -6,13 +6,19 @@ public class SpawnScript : MonoBehaviour {
 	public GameObject[] obj;
 	public float spawnMin = 1f;
 	public float spawnMax = 2f;
+	public int currentLane = 0;
+	public Transform[] spawnLoc;
+
 	// Use this for initialization
 	void Start () {
 		Spawn ();
 	}
 	
 	void Spawn () {
-		Instantiate(obj[Random.Range (0, obj.GetLength(0))], transform.position, Quaternion.identity);
+
+
+
+		Instantiate(obj[Random.Range ((int)0, obj.GetLength(0))], spawnLoc[(int)Random.Range(0,spawnLoc.GetLength(0))].position, Quaternion.identity);
 		Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
 		
 	}
