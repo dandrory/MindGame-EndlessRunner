7037601897c92151874c0eddcc45c18975bb756b
dan drory <dan.drory@hva.nl>
2014-03-19 11:04:53 +0100

    PG hallway, Jump fix, debouncing

---
+++ UnityProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform player;
+	public float zHeight = 1;
+	public float trailDistance = 7;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(player.position.x,zHeight, player.position.z - trailDistance);
+		transform.LookAt(player);
+	}
+}
---
+++ UnityProject/Assets/Scripts/HallGenerator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HallGenerator : MonoBehaviour {
+
+	public GameObject[] blocks;
+	public Vector3 baseLoc = new Vector3(0,-0.6581007f,0);
+	public Vector3 spawnLoc;
+	public Vector3 baseRot = new Vector3(270, 90, 0);
+
+	public float length;
+
+
+	// Use this for initialization
+	void Start () {
+		Generate(0);
+	}
+	
+	public void Generate(float zLoc){
+		if(zLoc > length){
+			Debug.Log("Done building");
+		}
+		else{
+			Instantiate(blocks[getBlockType()], new Vector3(0,baseLoc.y,zLoc), Quaternion.Euler(baseRot));
+			Generate(zLoc + 10);
+		}
+	}
+
+	public void SpawnSingle(float zLoc){
+		Instantiate(blocks[getBlockType()], new Vector3(0,baseLoc.y,zLoc), Quaternion.Euler(baseRot));
+	}
+	
+
+	public int getBlockType(){
+		return 0;
+	}
+}
